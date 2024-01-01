63abe1b64985bd66c704b4fc76a2871fb5dfc387
Roslin-v <roslin_v@163.com>
2024-01-01 16:47:50 +0800

    add framework

---
+++ model/model.py
@@ -0,0 +1,24 @@
+# Trajectory Planning Network
+class TPN():
+    def __init__(self):
+        self.node_attn_net()
+        self.heuristics_net()
+
+    # 可观测成本g
+    def node_attn_net(self):
+        # ========== 根据交通网络图计算路线距离
+        # ========== 根据已有轨迹计算路线吸引力
+        # ========== 两部分合起来计算可观测成本
+        print('Finished node attention network.')
+
+    # 估计成本h
+    def heuristics_net(self):
+        # ========== 利用图神经网络估计到目的地的代价
+        # ========== 引入多头注意力机制
+        print('Finished heuristics network.')
+
+
+# Improved A Star Algorithm using TPN
+class AStarPlus():
+    def __init__(self):
+        print('Finished A Star Algorithm.')
---
+++ model/train.py
@@ -0,0 +1,20 @@
+from data_process import *
+
+
+def train(model, args):
+    # ========== 加载数据集
+    print('----------Loading data----------')
+    # ========== 数据预处理
+    # Step 1: 生成图
+    traj_df = pd.read_csv(os.path.join('../data/traj.csv'))
+    print('----------Building trajectory graph ----------')
+    G = build_traj_graph(traj_df)
+    traj2csv(G)
+    trans_df = pd.read_csv(os.path.join('../data/transportation.csv'), encoding='ANSI')
+    print('----------Building transportation graph ----------')
+    G = build_trans_graph(trans_df)
+    trans2csv(G)
+    # Step 2: 生成字典
+    # Step 3: 上下文嵌入
+    # ========== 利用时间差分法训练网络
+    print('----------Training----------')
